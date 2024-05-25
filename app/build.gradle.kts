57e55a2f8e5ac3565f52cca23a8bc383b02588e7
Mohamad Abuzaid <mabuzaid@sure.com.sa>
2024-05-25 23:54:36 +0300

    - Added Launcher Icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,18 +4,60 @@ plugins {
     alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.jetbrainsCompose)
 }
-apply(from = rootProject.file("gradle/config-properties.gradle"))
-apply(from = rootProject.file("gradle/config-android.gradle"))
 
 android {
     namespace = "me.abuzaid.movies"
 
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        applicationId = "me.abuzaid.movies"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = libs.versions.android.version.code.get().toInt()
+        versionName = libs.versions.android.version.name.get()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            //shrinkResources = true
+            isDebuggable = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
+        }
+
+        create("staging") {
+            isDebuggable = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            matchingFallbacks += listOf("release", "debug")
+        }
+
+        getByName("debug") {
+            isDebuggable = true
+            extra["enableCrashlytics"] = false
+        }
+    }
+
     buildFeatures {
+        buildConfig = true
         compose = true
     }
 
     composeOptions {
         kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
