dfaaf8745a44d6ca0e70a2ee37305cca7bdeeb92
Mohamad Abuzaid <mabuzaid@sure.com.sa>
2024-05-26 07:17:49 +0300

    - Removing OkHttp

--- data/src/main/java/me/abuzaid/movies/data/network/di/NetworkModules.kt
+++ data/src/main/java/me/abuzaid/movies/data/network/di/NetworkModules.kt
@@ -1,7 +1,5 @@
 package me.abuzaid.movies.data.network.di
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import me.abuzaid.movies.data.BuildConfig
 import me.abuzaid.movies.data.network.factory.KtorClientFactory
 import org.koin.core.qualifier.named
@@ -12,22 +10,13 @@ import org.koin.dsl.module
  * Email: m.abuzaid.ali@gmail.com
  */
 val networkModule = module {
-    includes(okhttpModule)
-
     single(named("dataBaseUrl")) { BuildConfig.BASE_URL }
     single(named("accessToken")) { BuildConfig.TOKEN }
-
-    single<Moshi> {
-        Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-    }
 
     single {
         KtorClientFactory.getInstance(
             baseUrl = get(named("dataBaseUrl")),
-            okHttpClient = get(),
-            parser = get()
+            token = get(named("accessToken"))
         )
     }
 }
--- data/src/main/java/me/abuzaid/movies/data/network/factory/KtorFactory.kt
+++ data/src/main/java/me/abuzaid/movies/data/network/factory/KtorFactory.kt
@@ -1,8 +1,7 @@
 package me.abuzaid.movies.data.network.factory
 
-import android.util.Log
-import com.squareup.moshi.Moshi
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.*
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -12,18 +11,15 @@ import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.observer.ResponseObserver
 import io.ktor.client.request.accept
+import io.ktor.client.request.bearerAuth
 import io.ktor.client.request.header
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
+import io.ktor.http.URLProtocol
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
-import okhttp3.OkHttpClient
-import io.ktor.client.*
-import io.ktor.client.engine.okhttp.*
-import io.ktor.client.features.json.*
-import io.ktor.client.features.json.serializer.*
-import io.ktor.client.features.logging.*
+import timber.log.Timber
 
 /**
  * Created by "Mohamad Abuzaid" on 25/05/2024.
@@ -34,65 +30,61 @@ object KtorClientFactory {
 
     fun getInstance(
         baseUrl: String,
-        okHttpClient: OkHttpClient,
-        parser: Moshi
+        token: String
     ): HttpClient {
         synchronized(this) {
-            return HttpClient(OkHttp) {
-                engine {
-                    preconfigured = okHttpClient
+            return HttpClient(Android) {
+                install(ContentNegotiation) {
+                    json(
+                        Json {
+                            prettyPrint = true
+                            isLenient = true
+                            useAlternativeNames = true
+                            ignoreUnknownKeys = true
+                            encodeDefaults = false
+                        }
+                    )
                 }
-                install(JsonFeature) {
-                    serializer = MoshiSerializer(parser)
+
+                install(HttpTimeout) {
+                    requestTimeoutMillis = NETWORK_TIME_OUT
+                    connectTimeoutMillis = NETWORK_TIME_OUT
+                    socketTimeoutMillis = NETWORK_TIME_OUT
                 }
+
                 install(Logging) {
-                    level = LogLevel.INFO
+                    logger = object : Logger {
+                        override fun log(message: String) {
+                            Timber.v("Logger Ktor =>", message)
+                        }
+                    }
+                    level = LogLevel.ALL
+                }
+
+                install(ResponseObserver) {
+                    onResponse { response ->
+                        Timber.tag("HTTP status:").d(response.status.value.toString())
+                    }
+                }
+
+                install(DefaultRequest) {
+                    header(
+                        HttpHeaders.ContentType,
+                        ContentType.Application.Json
+                    )
+                }
+
+                defaultRequest {
+                    host = baseUrl
+                    url {
+                        protocol = URLProtocol.HTTPS
+                    }
+
+                    bearerAuth(token = token)
+                    contentType(ContentType.Application.Json)
+                    accept(ContentType.Application.Json)
                 }
             }
         }
-    }
-}
-
-val httpClientAndroid = HttpClient(Android) {
-    install(ContentNegotiation) {
-        json(
-            Json {
-                prettyPrint = true
-                isLenient = true
-                useAlternativeNames = true
-                ignoreUnknownKeys = true
-                encodeDefaults = false
-            }
-        )
-    }
-
-    install(HttpTimeout) {
-        requestTimeoutMillis = NETWORK_TIME_OUT
-        connectTimeoutMillis = NETWORK_TIME_OUT
-        socketTimeoutMillis = NETWORK_TIME_OUT
-    }
-
-    install(Logging) {
-        logger = object : Logger {
-            override fun log(message: String) {
-                Log.v("Logger Ktor =>", message)
-            }
-        }
-        level = LogLevel.ALL
-    }
-
-    install(ResponseObserver) {
-        onResponse { response ->
-            Log.d("HTTP status:", "${response.status.value}")
-        }
-    }
-
-    install(DefaultRequest) {
-        header(HttpHeaders.ContentType, ContentType.Application.Json)
-    }
-
-    defaultRequest {
-        contentType(ContentType.Application.Json)
-        accept(ContentType.Application.Json)
     }
 }
