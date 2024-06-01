00de2d903d113dc4e3c6dddf89db6329c54af88f
Mohamad Abuzaid <mabuzaid@sure.com.sa>
2024-06-01 20:37:14 +0300

    - Built Splash Screen

--- app/src/main/java/me/abuzaid/movies/navigation/MainNavGraph.kt
+++ app/src/main/java/me/abuzaid/movies/navigation/MainNavGraph.kt
@@ -2,7 +2,11 @@ package me.abuzaid.movies.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import me.abuzaid.movies.ui.screens.SplashScreen
+import me.abuzaid.movies.utils.storage.ILocalPreferencesStorage
+import org.koin.compose.koinInject
 
 /**
  * Created by "Mohamad Abuzaid" on 01/06/2024.
@@ -15,6 +19,10 @@ fun NavGraphBuilder.mainNavGraph(
         route = Graph.MAIN,
         startDestination = MainScreens.Splash.route
     ) {
+        composable(route = MainScreens.Splash.route) {
+            val prefs: ILocalPreferencesStorage = koinInject()
 
+            SplashScreen(navController, prefs)
+        }
     }
 }
