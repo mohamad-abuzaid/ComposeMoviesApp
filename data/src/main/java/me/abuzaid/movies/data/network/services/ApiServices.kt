eacb653c2004efd7f92443d32c9a9b9018c42001
Mohamad Abuzaid <mabuzaid@sure.com.sa>
2024-06-08 21:01:55 +0300

    - Update Key

--- data/src/main/java/me/abuzaid/movies/data/network/services/ApiServices.kt
+++ data/src/main/java/me/abuzaid/movies/data/network/services/ApiServices.kt
@@ -14,11 +14,11 @@ import me.abuzaid.movies.data.network.models.wrappers.RemoteResponse
 class ApiServices(private val client: HttpClient) {
 
     companion object {
-        private const val FETCH_MOVIES = "discover/movie"
+        private const val FETCH_POPULAR_MOVIES = "discover/movie"
     }
 
     suspend fun fetchPopularMovies(lang: String): RemoteResponse<List<MovieRemote>> =
-        client.get(FETCH_MOVIES) {
+        client.get(FETCH_POPULAR_MOVIES) {
             parameter("include_adult", false)
             parameter("include_video", false)
             parameter("language", lang)
